25e21398cce7ee3db3f749d6b076c44e89ff8b7f
eliezermoraesss <eliezer.moraes@outlook.com>
2022-09-22 23:30:37 -0300

    Added annotation @Data in UsuarioDTO

--- src/main/java/com/dsousa/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/dsousa/minhasfinancas/api/dto/UsuarioDTO.java
@@ -2,10 +2,12 @@ package com.dsousa.minhasfinancas.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Data
 @Getter
 @Setter
 @Builder
